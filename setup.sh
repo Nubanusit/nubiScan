cca7323d89fd238cecffe3fb695a7aea2dc19d48
cscottlamb <you@example.com>
2021-06-21 19:28:39 -0400

    Add .gitmodules and tweak the setup file.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 git submodule update --init --recursive
-sudo pip3 install -r requirments.txt
-sudo pip3 install -r kbdPicker/requirments.txt
+sudo apt install python3-pip
 sudo apt install ffmpeg
 sudo cp nubiScan.service /etc/systemd/system
+sudo pip3 install -r requirments.txt
+sudo pip3 install -r kbdPicker/requirments.txt
 sudo systemctl enable nubiScan
 sudo systemctl start nubiScan
